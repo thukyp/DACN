a7813b7430e5a35e3a9c1c7eefc6cb0ad04dd42f
Phan Thu Ky <103241882+thukyp@users.noreply.github.com>
2025-10-26 21:45:52 +0700

    Delete HenIshi provipmax/HenIshi directory
    Delete unused Henlshi directory

--- HenIshi provipmax/HenIshi/HenIshi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HenIshi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
